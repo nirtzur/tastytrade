4b6d6efc40316e148cfea0a3951e458d62364b96
Nir Tzur <tzurnir@gmail.com>
2025-05-19 22:10:03 +0300

    add home base route redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AccountHistoryTable from "./components/AccountHistoryTable";
 
 function App() {
   return (
     <div className="App">
-      <h1>Account History</h1>
-      <AccountHistoryTable />
+      <Routes>
+        <Route path="/" element={<Navigate to="/account-history" replace />} />
+        <Route path="/account-history" element={<AccountHistoryTable />} />
+      </Routes>
     </div>
   );
 }
