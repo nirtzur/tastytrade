4a9931e3fe236ee60f08aa1bfb5adf85abc68d2a
Nir Tzur <tzurnir@gmail.com>
2025-05-19 22:17:19 +0300

    add top menu bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,46 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { AppBar, Tabs, Tab, Box } from "@mui/material";
 import AccountHistoryTable from "./components/AccountHistoryTable";
+import VisualPage from "./components/VisualPage";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleTabChange = (event, newValue) => {
+    navigate(newValue);
+  };
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate to="/account-history" replace />} />
-        <Route path="/account-history" element={<AccountHistoryTable />} />
-      </Routes>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={location.pathname}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Account History" value="/account-history" />
+          <Tab label="Visual" value="/visual" />
+        </Tabs>
+      </AppBar>
+      <Box sx={{ p: 3 }}>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/account-history" replace />}
+          />
+          <Route path="/account-history" element={<AccountHistoryTable />} />
+          <Route path="/visual" element={<VisualPage />} />
+        </Routes>
+      </Box>
     </div>
   );
 }
---
+++ frontend/src/components/VisualPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VisualPage = () => {
+  return <div>{/* Empty screen for now */}</div>;
+};
+
+export default VisualPage;
