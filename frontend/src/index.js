672cdd43bacf53633f345aedcee43abe6ac6c761
Nir Tzur <tzurnir@gmail.com>
2025-05-25 20:00:18 +0300

    typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const theme = createTheme();
 
