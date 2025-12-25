7a0d44b046bd2f2485f22c4529c1def369663658
Nir Tzur <tzurnir@gmail.com>
2025-12-25 17:45:01 +0200

    force using ipv4

--- config/database.js
+++ config/database.js
@@ -16,6 +16,7 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: "mysql",
     dialectOptions: {
+      family: 4,
       ssl: {
         require: true,
         rejectUnauthorized: false,
