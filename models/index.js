aec8d71c39eb1cd78cc382bce6218b2a5aeaf091
Nir Tzur <tzurnir@gmail.com>
2025-12-25 18:18:51 +0200

    fixing sequelize params when on prod

--- models/index.js
+++ models/index.js
@@ -4,21 +4,28 @@ const config = require("../config/database");
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
+let sequelize;
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
     logging: env === "development" ? console.log : false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+    ...dbConfig,
+  });
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      logging: env === "development" ? console.log : false,
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+      ...dbConfig,
+    }
+  );
+}
 
 module.exports = sequelize;
